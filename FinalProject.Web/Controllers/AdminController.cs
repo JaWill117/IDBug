f6d2442857db3802a748f081e027adb458632d14
Jason W <williams.jasonmd@gmail.com>
2015-09-17 00:02:41 -0500

    add all pest pic and discription

---
+++ FinalProject.Web/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using FinalProject.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProject.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(db.Questions.ToList());
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(db.Questions.Find(id));
+        }
+    }
+}
